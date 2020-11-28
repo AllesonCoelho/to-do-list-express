6ac2d1c6cf23cedefcfa6099cbfa8d944195a3d0
Alleson Henrique <alleson_henrique@hotmail.com>
2020-11-28 19:29:10 -0300

    Concentrando Rotas

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
-
+const checkListRouter = require('./src/routes/checklist') 
 const app = express()
 
-app.get('/', (req, res)=> {
-    res.send('<h1>Minha lista de tarefas :)</h1>')
-})
+app.use(express.json())
+
+app.use('/checkLists',checkListRouter)
 
 app.listen(3000, () => {
     console.log('Servidor foi iniciado')
