b31d559af853ef599868f543af253ab05d765e4d
Alleson Henrique <alleson_henrique@hotmail.com>
2020-12-01 17:24:54 -0300

    Formulario com post

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -12,19 +12,32 @@ router.get('/', async (req, res) =>{
         res.status(200).render('checklists/index', {checklists : checklists})
         
     } catch (error) {
-        res.status(200).render('/pages/error', {error: "Erro ao exibir as listas"})
+        res.status(200).render('pages/error', {error: "Erro ao exibir as listas"})
         
     }
 })
 
-router.post('/', async (req, res) =>{
-    let {name} = req.body
+router.get('/new', async (req, res) => {
     try {
-        let checklist = await Checklist.create({name})
-        res.status(200).json(checklist)
+        let checklist = new Checklist()
+        res.status(200).render('checklists/new', {checklist : checklist})
         
     } catch (error) {
-        res.status(422).json(error)
+        res.status(500).render('pages/error', {errors:"Erro ao carregar o formulario"})
+        
+    }
+
+})
+
+router.post('/', async (req, res) =>{
+    let {name} = req.body.checklist
+    let checklist = new Checklist({name})
+    try {
+        await checklist.save()
+        res.redirect('/checklists')
+        
+    } catch (error) {
+        res.status(422).render('checklists/new', {checklists: {...checklist, error}})
         
     }
     
